c94e09a2221cbfeef6c8886b9b7cbaba9390c0c6
Jeremy Baumont <jeremy.baumont@gmail.com>
2016-01-14 23:04:52 +1100

    Change to just install gvm script

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,31 +2,17 @@
 class gvm(
   $version = $gvm::params::version
 ) inherits gvm::params {
-  
-  case $ensure {
-    present: {
-      # get the download URI
-      $download_uri = "https://raw.githubusercontent.com/moovweb/gvm/${version}/binscripts/gvm-installer"
 
-      $install_command = join([
-        "bash <<(curl -s -S -L ${download_uri})"
-      ], ' && ')
+  # get the download URI
+  $download_uri = "https://raw.githubusercontent.com/moovweb/gvm/${version}/binscripts/gvm-installer"
 
-      exec {
-        "install gvm v${version}":
-          command => $install_command,
-          #unless  => "test -x ~/.gvm && gvm version| grep '\\bv${version}\\b'",
+  $install_command = join([
+    "bash <<(curl -s -S -L ${download_uri})"
+  ], ' && ')
+
+  exec {
+    "install gvm v${version}":
+      command => $install_command,
+      #unless  => "test -x ~/.gvm && gvm version| grep '\\bv${version}\\b'",
       }
-    }
-
-    absent: {
-      file { $root:
-        ensure  => absent,
-        recurse => true,
-        force   => true,
-      }
-    }
-
-  }
-
 }
